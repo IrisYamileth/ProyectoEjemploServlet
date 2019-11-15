1a072423425296e8e6de5dc3fb8f1738b9129cdc
IrisGarcia <girisyamiilet@gmail.com>
2019-11-14 21:59:15 -0600

    Ejemplo Servlet1 actualizacion2 metodo get con url

--- JavaSeryJSPEE/CursoJSyJSP/Servlet/src/java/sv/irisgarcia/servlets/Comentarios.java
+++ JavaSeryJSPEE/CursoJSyJSP/Servlet/src/java/sv/irisgarcia/servlets/Comentarios.java
@@ -10,75 +10,84 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- *
- * @author Daysi Sofia Garcia
- */
 public class Comentarios extends HttpServlet {
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        //para mandar parametros a una URL Se pone signo de interrogacion y despues se ponen los parametros 
+        //ejemplo http://localhost:8080/servlets/comentarios?name=alonso&perfil=admin&id=4852
+
+        //1. recibimos los parametros del formulario
+        String name = request.getParameter("name");
+        String perfil = request.getParameter("perfil");
+        String id = request.getParameter("id");
+
+        //2 aplicamos las reglas del negocio con la informacion enviada por el usuario
+        //en este caso solo se van imprimir en pantalla, pero una regla de negocio podria ser guardar datos en bd
+        //hacer calculos con los datos, etc.
+        System.out.println("Name " + name);
+        System.out.println("Perfil " + perfil);
+        System.out.println("Id " + id);
+
+        //3. siempre regresamos una respuesta al cliente.en este caso en un documento HTML
+        if (perfil.equals("admin")) {
+            response.setContentType("text/html;charset=UTF-8");
+            try (PrintWriter out = response.getWriter()) {
+                /* TODO output your page here. You may use following sample code. */
+                out.println("<!DOCTYPE html>");
+                out.println("<html>");
+                out.println("<head>");
+                out.println("<title>Comentarios</title>");
+                out.println("</head>");
+                out.println("<body>");
+                out.println("<h3>Acceso concedido</h3>");
+                out.println("</body>");
+                out.println("</html>");
+            }
+        } else {
+            response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
+        }
+    }
+
+    @Override
+    //HttpServletRequest request es la peticion
+    //HttpServletResponse response es la respuesta
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        //1. recibimos los parametros del formulario
+        //el valor entre parentesis es el que se declara en el archivo comentarios.html
+        //en el formulario en el atributo name de cada valor ("name")
+        String nombreParam = request.getParameter("nombre");
+        String emailParam = request.getParameter("email");
+        String gradoParam = request.getParameter("grado");
+        String comentariosParam = request.getParameter("comentarios");
+
+        //2 aplicamos las reglas del negocio con la informacion enviada por el usuario
+        //en este caso solo se van imprimir en pantalla, pero una regla de negocio podria ser guardar datos en bd
+        //hacer calculos con los datos, etc.
+        System.out.println("Name " + nombreParam);
+        System.out.println("Perfil " + emailParam);
+        System.out.println("Id " + gradoParam);
+        System.out.println("Id " + comentariosParam);
+
+        //3. siempre regresamos una respuesta al cliente.en este caso en un documento HTML
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
             /* TODO output your page here. You may use following sample code. */
             out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
-            out.println("<title>Servlet Comentarios</title>");            
+            out.println("<title>Comentarios</title>");
             out.println("</head>");
             out.println("<body>");
-            out.println("<h1>Servlet Comentarios concatenado con getContext  " + request.getContextPath() + "</h1>");
+            out.println("<h3>Gracias por tus comentarios "+ nombreParam + "</h3>");
+            out.println("<h4>En breve nos comunicaremos contigo </h4>");
             out.println("</body>");
             out.println("</html>");
         }
     }
-
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
-    }
-
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
-    }
-
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
+// </editor-fold>
 
 }
